e4801d1f6689a2f9b99d9af57c77478e01c920e9
Thaynara Keiko Oda Santos <tkos.94@gmail.com>
2022-06-17 14:11:52 -0300

    Implementação de Usuário

---
+++ IES300.API.Domain/Interfaces/Services/IUsuarioService.cs
@@ -0,0 +1,15 @@
+﻿using IES300.API.Domain.DTOs.Usuario;
+using System.Collections.Generic;
+
+namespace IES300.API.Domain.Interfaces.Services
+{
+    public interface IUsuarioService
+    {
+        UsuarioOutputDTO InserirUsuario(UsuarioInsertDTO UsuarioInput);
+        List<UsuarioOutputDTO> ObterTodosUsuarios(bool ativado = true);
+        UsuarioOutputDTO ObterUsuarioPorId(int id);
+        void DeletarUsuario(int id);
+        UsuarioOutputDTO AlterarUsuario(UsuarioUpdateDTO usuarioInput);
+        void ValidarUsuario(UsuarioValidateDTO usuarioInput);
+    }
+}
