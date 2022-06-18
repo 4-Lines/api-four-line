23beb75512c7249cb4380003fddcdd882cbefd2d
FabianoPretoJr <61958796+FabianoPretoJr@users.noreply.github.com>
2022-06-18 15:17:03 -0300

    atualizacao de migration

--- IES300.API.Repository/Migrations/20220618181426_correcaoMapUsuario.Designer.cs
+++ IES300.API.Repository/Migrations/20220618181426_correcaoMapUsuario.Designer.cs
@@ -3,14 +3,16 @@ using IES300.API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IES300.API.Repository.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220618181426_correcaoMapUsuario")]
+    partial class correcaoMapUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
